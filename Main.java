2ace485ef906f497f95d6d1f3ec73a3395d534f5
ar80832 <69652822+ar80832@users.noreply.github.com>
2020-08-13 14:21:28 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!");
+        System.out.println("Hi, my name is Aditi.");
+        System.out.println("My hobbies include swimming, reading, and sleeping.");
+        System.out.println("I really like to watch tv shows in my free time as well.");
+        System.out.println("My favorite show right now is The Originals.");
+        System.out.println("I am also in band where I play the flute.");
+    }
+}
